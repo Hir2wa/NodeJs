50c42aff59ef10434b5be4beceb83c8d0ea18346
Hir2wa <alainfabricehirwa@gmail.com>
2025-09-16 09:17:48 +0200

    added server request

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require("http");
+const fs = require("fs");
+const { log } = require("console");
 const server = http.createServer((req, res) => {
-  console.log("Request Made ");
+  console.log(req.url, req.method);
+
+  //setting header
+  res.setHeader("contentType", "text/plain");
+
+  fs.readFile("./index.html", (error, data) => {
+    if (error) {
+      console.log("Opps Something Happens");
+    }
+  });
 });
 
-server.listen(3, "localhost", () => {
-  console.log("port running on  3");
+server.listen(4, "localhost", () => {
+  console.log("port running on  4");
 });
