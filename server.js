ad260fe2806963cfd402ba40d26ba1b70b07a2c8
Hir2wa <alainfabricehirwa@gmail.com>
2025-09-16 10:10:47 +0200

    added status check code

--- server.js
+++ server.js
@@ -8,7 +8,26 @@ const server = http.createServer((req, res) => {
   res.setHeader("contentType", "text/html");
   // res.write("Hello Alain");
   // res.end();
-  fs.readFile("./index.html", (err, data) => {
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
