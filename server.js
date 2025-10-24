baedc44224284d3205dcc85cc5ead0aaa5d0d9c6
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-24 10:18:16 +0200

    update packages  with a new version

--- server.js
+++ server.js
@@ -1,54 +1,90 @@
-const http = require("http");
-const fs = require("fs");
-const _ = require("lodash");
-const { log } = require("console");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  const num = _.random(0, 70);
-  console.log(num);
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
 
-  //setting header
-  res.setHeader("Content-Type", "text/html");
-  // res.write("Hello Alain");
-  // res.end();
+// custom middleware logger
+app.use(logger);
 
-  let path = "./views/";
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      path += "about.html";
-      res.statusCode = 200;
-      break;
-    case "/about-me":
-      res.statusCode = 301;
-      res.setHeader("location", "/about");
-      res.end();
-      break;
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
 
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-      break;
-  }
+// built-in middleware for json 
+app.use(express.json());
 
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
     } else {
-      // res.write(data);
-      res.end(data);
+        res.type('txt').send("404 Not Found");
     }
-  });
 });
 
-server.listen(3001, "localhost", () => {
-  console.log("port running on 3001");
-});
+app.use(errorHandler);
 
-// express is a frameworkd that help use to manage request
-// routing request server side
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
