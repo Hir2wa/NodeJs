2c5988b443027a2de0b3f06c09a87f4fe74c8bc1
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-25 13:10:51 +0200

    added controller file supporting api request

---
+++ controllers/employeesController.js
@@ -0,0 +1,36 @@
+const data = {};
+data.employees = require("../modal/employees.json");
+
+const getAllEmployees = (req, res) => {
+  res.json(data.employees);
+};
+
+const addEmployee = (req, res) => {
+  res.json({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  });
+};
+
+const udpateEmployee = (req, res) => {
+  res.json({
+    firstname: req.body.lastname,
+    lastname: req.body.lastname,
+  });
+};
+
+const deleteEmployee = (req, res) => {
+  res.json({ id: req.body.id });
+};
+
+const getEmployee = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+module.exports = {
+  getAllEmployees,
+  deleteEmployee,
+  udpateEmployee,
+  addEmployee,
+  getEmployee,
+};
