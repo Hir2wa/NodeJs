c8e3862168a97090b8edc2ee953b46d3e3190a9c
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-25 12:05:43 +0200

    added api folder for integrating api

--- routes/subdir.js
+++ routes/subdir.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const path = require("path");
 
 router.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "subdir", "views", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "index.html"));
 });
 
+router.get("^/test(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "test.html"));
+});
 module.exports = router;
