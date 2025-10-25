a8e596d13132b7f3dfd5ba172d1b61b90e3e64b0
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-25 12:25:23 +0200

    removed unwanted import

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+
 const data = {};
 data.employees = require("../../data/employees.json");
 
@@ -28,4 +28,7 @@ router
     res.json({ id: req.body.id });
   });
 
+router.route("/:id").get((req, res) => {
+  res.json({ id: req.params.id });
+});
 module.exports = router;
