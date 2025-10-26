571201668af6f5ff3b4486c3ce25cb0177b5a202
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-26 23:06:35 +0200

    Additional updates to registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -8,6 +8,7 @@ const fsPromises = require("fs").promises;
 const path = require("path");
 const bcrypt = require("bcrypt");
 const { use } = require("react");
+const { json } = require("stream/consumers");
 
 const handleNewUser = async (req, res) => {
   const { user, psd } = req.body;
@@ -18,6 +19,15 @@ const handleNewUser = async (req, res) => {
 
   try {
     const hashedPassword = await bcrypt.hash(psd, 10);
+    const newUser = { username: user, password: psd };
+    userDB.setUser([...userDB.user, newUser]);
+
+    await fsPromises.writeFile((req, res) => {
+      path.join("__dirname", "..", "modal", "user.json");
+      JSON.stringify(userDB.user);
+
+      res.status(201).json({ success: `New User: ${user} created!!` });
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
