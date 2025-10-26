5b688280cbc372c750ec742a25b807de8476e3ec
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-26 23:47:42 +0200

    Update registerController.js with latest changes

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,27 +7,25 @@ const userDB = {
 const fsPromises = require("fs").promises;
 const path = require("path");
 const bcrypt = require("bcrypt");
-const { use } = require("react");
-const { json } = require("stream/consumers");
 
 const handleNewUser = async (req, res) => {
   const { user, psd } = req.body;
   if (!user || !psd)
     return res.status(400).json({ message: "Invalid UserInfo" });
   const duplicate = userDB.user.find((person) => person.userName === user);
-  if (duplicate) return res.status(409).json({ message: "duplicate" }); //checking for duplicates
+  if (duplicate)
+    return res.status(409).json({ message: "username already exists" }); //checking for duplicates
 
   try {
     const hashedPassword = await bcrypt.hash(psd, 10);
-    const newUser = { username: user, password: psd };
+    const newUser = { username: user, password: hashedPassword };
     userDB.setUser([...userDB.user, newUser]);
 
-    await fsPromises.writeFile((req, res) => {
-      path.join("__dirname", "..", "modal", "user.json");
-      JSON.stringify(userDB.user);
+    const filePath = path.join("__dirname", "..", "modal", "user.json");
+    await fsPromises.writeFile(filePath, JSON.stringify(userDB.user));
+    JSON.stringify(userDB.user);
 
-      res.status(201).json({ success: `New User: ${user} created!!` });
-    });
+    res.status(201).json({ success: `New User: ${user} created!!` });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
