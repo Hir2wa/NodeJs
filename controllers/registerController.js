d31d4b8b153801c7e00ea15145e818f587d62bf5
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-27 00:12:11 +0200

    Fix arrow function issue in registerController - change to regular function for proper 'this' context and add debugging logs

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,6 @@
 const userDB = {
   user: require("../modal/user.json"),
-  setUser: (data) => {
+  setUser: function (data) {
     this.user = data;
   },
 };
@@ -12,21 +12,23 @@ const handleNewUser = async (req, res) => {
   const { user, psd } = req.body;
   if (!user || !psd)
     return res.status(400).json({ message: "Invalid UserInfo" });
-  const duplicate = userDB.user.find((person) => person.userName === user);
+  const duplicate = userDB.user.find((person) => person.username === user);
   if (duplicate)
     return res.status(409).json({ message: "username already exists" }); //checking for duplicates
 
   try {
     const hashedPassword = await bcrypt.hash(psd, 10);
     const newUser = { username: user, password: hashedPassword };
+    console.log("New user to add:", newUser);
+    console.log("All User : ", userDB.user);
+
     userDB.setUser([...userDB.user, newUser]);
+    const filePath = path.join(__dirname, "..", "modal", "user.json");
+    await fsPromises.writeFile(filePath, JSON.stringify(userDB.user, null, 2));
 
-    const filePath = path.join("__dirname", "..", "modal", "user.json");
-    await fsPromises.writeFile(filePath, JSON.stringify(userDB.user));
-    JSON.stringify(userDB.user);
-
-    res.status(201).json({ success: `New User: ${user} created!!` });
+    res.status(201).json({ success: `New User: ${user} created!` });
   } catch (error) {
+    console.error("Error creating user:", error);
     res.status(500).json({ message: error.message });
   }
 };
