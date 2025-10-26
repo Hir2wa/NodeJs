74ae0e83dff1502e757a8371b0fdc904dccb911a
Hir2wa <alainfabricehirwa@gmail.com>
2025-10-26 22:20:05 +0200

    Update registerController, package.json and package-lock.json with latest changes

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,3 +6,12 @@ const userDB = {
 };
 const fsPromises = require("fs").promises;
 const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { user, psd } = req.body;
+  if (!user || !psd)
+    return res.status(400).json({ message: "Invalid UserInfo" });
+
+  //checking for duplicates
+};
