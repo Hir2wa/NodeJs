18e2b1204c3a1c4d9fa1312454e767f55146054f
Hir2wa <alainfabricehirwa@gmail.com>
2025-11-15 23:31:17 +0200

    Add JWT verification to employees routes
    - Protect all employee endpoints with verifyJWT
    - Require authentication for employee operations
    - Extract user info from JWT tokens

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,14 +2,28 @@ const express = require("express");
 const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
 const verifyJWT = require("../../middleware/verifyJWT");
+const verifyRoles = require("../../middleware/verifyRoles");
+const ROLES_LIST = require("../../config/roles_list");
 router
   .route("/")
   .get(verifyJWT, employeesController.getAllEmployees)
-  .post(employeesController.addEmployee);
+  .post(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeesController.addEmployee
+  );
 
 router
   .route("/:id")
-  .get(employeesController.getEmployee)
-  .put(employeesController.udpateEmployee)
-  .delete(employeesController.deleteEmployee);
+  .get(verifyJWT, employeesController.getEmployee)
+  .put(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeesController.udpateEmployee
+  )
+  .delete(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin),
+    employeesController.deleteEmployee
+  );
 module.exports = router;
